70343769dd82b336267093cba3afd05cf15d099d
rbsabin <sabin.ranabhat96@gmail.com>
2019-01-21 14:47:47 +0545

    Complete Authentication System

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -11,6 +13,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+
 
   end
 
@@ -64,5 +67,13 @@ class ArticlesController < ApplicationController
     def article_params
         params.require(:article).permit(:title, :description)    
     end
+
+    def require_same_user
+      if current_user != @article.user
+        flash[:danger] = "You can only edit or delete your own articles"
+        redirect_to root_path
+      end
+    end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
    
     before_action :set_user, only: [:edit, :update, :show]
+    before_action :require_same_user, only: [:edit, :update]
    
     def new
         @user = User.new
@@ -54,5 +55,11 @@ class UsersController < ApplicationController
 
     end
 
+    def require_same_user
+        if current_user  != @user
+            flash[:danger] = "You can only edit your own account"
+            redirect_to root_path
+        end
+    end
 
 end
