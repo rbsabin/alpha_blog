d57aa4c0e805c19a4ca44a32f236a27a9a991321
rbsabin <sabin.ranabhat96@gmail.com>
2019-01-21 14:50:19 +0545

    Update current_user to be article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,7 +21,7 @@ class ArticlesController < ApplicationController
       # render plain: params[:article].inspect
       debugger
       @article = Article.new(article_params)
-      @article.user = User.first
+      @article.user = current_user
       if @article.save
         #do something
         flash[:success] = "Article was successfully created"
